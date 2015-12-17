d929b61f6325edcf1cc6b15d04162341647686ef
Ines Alexandra Santos Carneiro <up201303501@linus10.(none)>
2015-12-17 20:38:20 +0000

    ftp funcional

---
+++ FTP/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+	<html>
+		<head>
+			<title>COUNT ME IN </title>
+			 <link type="text/css" rel="stylesheet" href="../CSS/login-form.css"/>
+			 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+			 <script src="../JS/login.js"></script>
+			 <link rel="stylesheet" type="text/css" href="../sweetalert-master/dist/sweetalert.css">
+			 <link href='https://fonts.googleapis.com/css?family=Oxygen:400,700,300' rel='stylesheet' type='text/css'>
+			 <link href='https://fonts.googleapis.com/css?family=Hind' rel='stylesheet' type='text/css'>
+		</head>
+		<body>
+			<div class="logo"></div>
+				<div class="login-block">
+					<form id="LogIn" >
+						<h1>Count me in</h1>
+						<input type="text" value="" placeholder="Username" id="username" required>					
+						<input type="password" value="" placeholder="Password" id="password" required>
+						<button type="submit" id="login">log in</button>
+					</form>
+						<a href="register.html"><button id="register">register</button></a>
+				</div>
+				<script src="../sweetalert-master/dist/sweetalert.min.js"></script> 
+		</body>
+	</html>
